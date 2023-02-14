d10ce77211958046c9eb114fff4b539774ea8ae5
AGUSTINA PATT <agustinapatt@gmail.com>
2023-02-14 11:44:23 +0100

    express sessions + log in working

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+require('./config/session.config')(app);
+
 // default value for title local
 const capitalize = require("./utils/capitalize");
 const projectName = "cornudify";
@@ -33,9 +35,6 @@ app.use("/", musicRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/", authRoutes);
-
-const authRoutes = require("./routes/auth.routes");
-app.use("/", authRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
