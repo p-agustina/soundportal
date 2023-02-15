f9df5aab8ceb75911c59f5a82f768470a937c4f3
Alex <alex.isaguilera@hotmail.com>
2023-02-15 12:08:46 +0100

    it shows options in the Navbar depending if user is logged or not

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const hbs = require("hbs");
 
 const app = express();
 
+
+
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
@@ -32,6 +34,14 @@ app.use(
     })
   })
 )
+
+//FOR SHOWING CONTENT IN HBS DEPENDING IF USER IS LOGGED IN
+
+app.use((req,res,next)=>{
+  if(req.isLoggedIn) res.locals.isLoggedIn = req.isLoggedIn();
+  next();
+});
+
 
 // default value for title local
 const capitalize = require("./utils/capitalize");
@@ -52,6 +62,8 @@ app.use("/", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+
 
 
 
